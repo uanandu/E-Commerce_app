8bbddc6c9ea1fba359355e744535eccfca79b2e9
Ryan Bajzath <R.bajzath@hotmail.com>
2022-07-30 17:17:19 -0400

    updated

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,23 +1,22 @@
-import { useState, useEffect } from 'react';
-import {BrowserRouter, Routes, Route} from "react-router-dom";
-import AboutUs from './AboutUs';
+import { useState, useEffect } from "react";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
+import AboutUs from "./AboutUs";
 
 const App = () => {
-
-return (
-  <BrowserRouter>
-    <Routes>
-      <Route path = "/" element={ "Homepage"}/>
-      <Route path = "/shop" element={ "Shop"}/>
-      <Route path = "/item/:itemId" element={ "Item"}/>
-      <Route path = "/checkout" element={ "Checkout"}/>
-      <Route path = "/category/:categoryId" element={ "Category"}/>
-      <Route path = "/company/:companyId" element={ "Company"}/>
-      <Route path = "/previous-purchases" element={ "Previous purchases"}/>
-      <Route path = "/about-us" element={<AboutUs/>}/>
-      </Routes>
-  </BrowserRouter>
-);
+  return (
+    <BrowserRouter>
+      <Switch>
+        <Route path="/" element={"Homepage"} />
+        <Route path="/shop" element={"Shop"} />
+        <Route path="/item/:itemId" element={"Item"} />
+        <Route path="/checkout" element={"Checkout"} />
+        <Route path="/category/:categoryId" element={"Category"} />
+        <Route path="/company/:companyId" element={"Company"} />
+        <Route path="/previous-purchases" element={"Previous purchases"} />
+        <Route path="/about-us" element={<AboutUs />} />
+      </Switch>
+    </BrowserRouter>
+  );
 };
 
 export default App;
