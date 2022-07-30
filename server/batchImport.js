9d0c216171ad5d285f22e1aa02ed9ccdd8440c5a
Ananda Narayanan Udayakumar <ananduun@icloud.com>
2022-07-30 12:07:03 -0400

    batchimport done.

---
+++ server/batchImport.js
@@ -0,0 +1,40 @@
+const {MongoClient} = require("mongodb");
+
+require("dotenv").config();
+const {MONGO_URI} = process.env;
+
+const options = { 
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+};
+
+const companies = require("./data/companies.json");
+const items = require("./data/items.json");
+
+// Here we import the data to the collections
+const batchImport = async () => {
+
+    try {
+
+        const client = new MongoClient(MONGO_URI, options); // Connect to the database using the URI and options
+
+        await client.connect(); // connect to the client
+
+        const db = client.db("groupProject"); // declare the database
+
+        const result = await db.collection("companies").insertMany(companies); // insert the companies into the database as a new collection
+
+        console.log("companies result here", result);
+
+        const result2 = await db.collection("all_items").insertMany(items); // insert the items into the database as a new collection
+
+        console.log("all items here", result2);
+
+        client.close();
+
+    } catch (error) {
+        console.log(error.stack, "error");
+    }
+}
+
+batchImport(); // call the function to import the data
