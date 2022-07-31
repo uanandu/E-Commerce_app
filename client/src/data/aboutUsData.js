03dc47753e900c791bc89e07a596ef5f1d1537ea
“Laura <laurakekesi@gmail.com>
2022-07-31 13:26:36 -0400

    about_us skeleton complete for component and data

---
+++ client/src/data/aboutUsData.js
@@ -0,0 +1,28 @@
+import lauraPic from "../styles/images/IMG_4614.jpeg";
+
+export const aboutUs = 
+    [{Info: [
+        {fullName: "Laura Kekesi" },
+        {imgSrc: lauraPic},
+        {linkedIn: "http://linkedin.com/in/laurakekesi"},
+        {gitHub: "http://github.com/laurakekesi"}
+    ]},
+    {Info: [
+        {fullName: "Laura Kekesi" },
+        {imgSrc: lauraPic},
+        {linkedIn: "http://linkedin.com/in/laurakekesi"},
+        {gitHub: "http://github.com/laurakekesi"}
+    ]},
+    {Info: [
+        {fullName: "Laura Kekesi" },
+        {imgSrc: lauraPic},
+        {linkedIn: "http://linkedin.com/in/laurakekesi"},
+        {gitHub: "http://github.com/laurakekesi"}
+    ]},
+    {Info: [
+        {fullName: "Laura Kekesi" },
+        {imgSrc: lauraPic},
+        {linkedIn: "http://linkedin.com/in/laurakekesi"},
+        {gitHub: "http://github.com/laurakekesi"}
+    ]}
+]
