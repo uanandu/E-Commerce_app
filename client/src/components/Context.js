9c95c9e7d6a15b26168dc747f352e2b36b2733f3
Ananda Narayanan Udayakumar <ananduun@icloud.com>
2022-07-31 17:08:09 -0400

    fetching the data inside the context

--- client/src/components/Context.js
+++ client/src/components/Context.js
@@ -2,14 +2,39 @@ import { createContext, useState } from "react";
 
 export const Context = createContext();
 
-export const ContextProvider = ({children}) => {
+export const ContextProvider = ({ children }) => {
+  const [items, setItems] = useState(null);
+  const [categories, setCategories] = useState(null);
+  const [companies, setCompanies] = useState(null);
+  const [cart, setCart] = useState(null);
+  const [orderHistory, setOrderHistory] = useState(null);
 
+  useEffect(() => {
+    axios
+      .get("/api/companies")
+      .then((res) => {
+        // console.log(res);
+        setCompanies(res.data);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }, []);
 
-return (
-    <Context.Provider
-    value = {{}}>
-        {children}
-    </Context.Provider>
-    );
+  useEffect(() => {
+    axios
+      .get("/api/items")
+      .then((res) => {
+        // console.log(res);
+        setItems(res.data);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }, []);
 
+  console.log("items here", items);
+  console.log("companies here", companies);
+
+  return <Context.Provider value={{}}>{children}</Context.Provider>;
 };
