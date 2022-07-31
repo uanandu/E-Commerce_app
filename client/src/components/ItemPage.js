324081e63f68dc5aa60e2f483154cdbc039f569e
Ryan Bajzath <R.bajzath@hotmail.com>
2022-07-31 06:42:26 -0400

    created a ItemPage.js

---
+++ client/src/components/ItemPage.js
@@ -0,0 +1,12 @@
+// this page will show the indivdual items information after being clicked on/
+import React from "react";
+
+const ItemPage = () => {
+  return (
+    //1st step fetch the data of the item based on the :param (item id)
+    //2nd step is to render it.
+    <div>ItemPage</div>
+  );
+};
+
+export default ItemPage;
