92c2b1c028e25eb19cb78d2b45fa3823bb936a0e
“Jingwen <xiajingwen77@gmail.com>
2022-07-31 14:31:56 -0400

    backenddone

---
+++ server/handler_cart.js
@@ -0,0 +1,52 @@
+const { MongoClient } = require("mongodb");
+require("dotenv").config();
+const { MONGO_URI } = process.env;
+
+const options = {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+};
+
+const getCart = async (req, res) => {
+  const client = new MongoClient(MONGO_URI, options);
+  await client.connect();
+  const db = client.db("groupProject");
+  const cart = await db.collection("cart").find().toArray();
+  cart? 
+    res.status(200).json({ status: 200, data: cart, message: "getCart success!" })
+    : res.status(404).json({ status: 404, message: "getCart fail!" });
+  client.close();
+};
+
+const postCart = async (req, res) => {
+  const client = new MongoClient(MONGO_URI, options);
+
+  await client.connect();
+  const db = client.db("groupProject");
+
+  //to define how to get new added item info in cart
+  //eg. console.log(req.body)
+  const newInCart = await db.collection("cart").insertone(req.body);
+  newInCart? res.status(200).json({status: 200,data: newInCart,message: "add newInCart success!",})
+    : res.status(404).json({ status: 404, message: "add newInCart fail!" });
+client.close();
+};
+
+const deleteCart = async (req, res) => {
+    const client = new MongoClient(MONGO_URI, options);
+    await client.connect();
+    const db = client.db("groupProject");
+    //this part needs connect item added in cart writes _id in cart collection
+    const _id = req.body.itemId;
+    //delete item based on _id in cart collection
+    const itemToDelete = await db.collection("cart").findOne({ _id });
+    !itemToDelete
+      ? res.status(404).json({ status: 404, message: "deleteCart fail,item not in cart" }):
+      await db.collection("cart").deleteOne({_id})
+      res.status(200).json({status: 200,data: _id,message: "deleteCart success!"})
+  client.close();
+  };
+
+module.exports = {
+  getCart,postCart,deleteCart
+};
---
+++ server/handler_history.js
@@ -0,0 +1,47 @@
+const { MongoClient } = require("mongodb");
+require("dotenv").config();
+const { MONGO_URI } = process.env;
+
+const options = {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+};
+
+const getOrderHistory = async (req, res) => {
+  const client = new MongoClient(MONGO_URI, options);
+
+  await client.connect();
+  const db = client.db("groupProject");
+  const orderHistory = await db.collection("purchasedItems").find().toArray();
+  orderHistory? 
+    res.status(200).json({ status: 200, data: orderHistory, message: "getOrderHistory success!" })
+    : res.status(404).json({ status: 404, message: "getOrderHistory fail!" });
+
+  client.close();
+};
+
+const postOrderHistory = async (req, res) => {
+  const client = new MongoClient(MONGO_URI, options);
+
+  await client.connect();
+  const db = client.db("groupProject");
+
+  //to define how to get new added item info in cart
+  //eg. console.log(req.body)
+  const newInHis = await db.collection("purchasedItems").insertone(req.body);
+  newInHis
+    ? res
+        .status(200)
+        .json({
+          status: 200,
+          data: newInHis,
+          message: "add newInHis success!",
+        })
+    : res.status(404).json({ status: 404, message: "add newInHis fail!" });
+client.close();
+};
+
+
+module.exports = {
+    getOrderHistory,postOrderHistory
+};
